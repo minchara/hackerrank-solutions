4f071c8a10c34801bc365de19442d8419738c2f6
Minchara H V <minchara48@gmail.com>
2026-08-23 20:36:55 +0530

    ✅  Java Stdin and Stdout I [java] — approach1

---
+++ solutions/java/easy/java-stdin-and-stdout-1/solution-approach1.java
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+// Problem      Java Stdin and Stdout I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-10, 02:38 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
