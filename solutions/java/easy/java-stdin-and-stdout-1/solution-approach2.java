3e7e5d83811bae11b754e42e914a2e27f3ea4252
Minchara H V <minchara48@gmail.com>
2026-08-23 20:44:05 +0530

    ✅  Java Stdin and Stdout I [java] — approach2

--- solutions/java/easy/java-stdin-and-stdout-1/solution-approach2.java
+++ solutions/java/easy/java-stdin-and-stdout-1/solution-approach2.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java
 // Status      Accepted
-// Submitted   2026-08-23, 08:43 p.m.
+// Submitted   2026-08-23, 08:44 p.m.
 // ──────────────────────────────────────────────────
 
 import java.util.*;
