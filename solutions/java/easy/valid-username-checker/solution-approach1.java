b1102cd42c8c051fe1a40fafc36fa26f20fbc0bb
Minchara H V <minchara48@gmail.com>
2026-08-31 09:35:54 +0530

    ✅ Valid Username Regular Expression [java] — approach1

---
+++ solutions/java/easy/valid-username-checker/solution-approach1.java
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+// Problem     Valid Username Regular Expression
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-31, 09:35 a.m.
+// ──────────────────────────────────────────────────
+
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
