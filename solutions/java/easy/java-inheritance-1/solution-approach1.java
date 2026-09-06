ab9deb366465cc8f2890a074b7da40e9ed6e0891
Minchara H V <minchara48@gmail.com>
2026-09-06 18:54:23 +0530

    ✅ Java Inheritance I [java] — approach1

---
+++ solutions/java/easy/java-inheritance-1/solution-approach1.java
@@ -0,0 +1,22 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-inheritance-1/problem?isFullScreen=true
+// Problem     Java Inheritance I
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-06, 06:54 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
