9821fd32f935356cbe5a2c667dff452ade066a31
Minchara H V <minchara48@gmail.com>
2026-09-06 18:48:42 +0530

    ✅ Java 1D Array [java] — approach1

---
+++ solutions/java/easy/java-1d-array-introduction/solution-approach1.java
@@ -0,0 +1,16 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+// Problem     Java 1D Array
+// Difficulty  Easy
+// Subdomain   Data Structures
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-06, 06:48 p.m.
+// ──────────────────────────────────────────────────
+
+
+int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
