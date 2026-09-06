b765cb2bf01bb3d7c08059aa27353383ac0005c2
Minchara H V <minchara48@gmail.com>
2026-09-06 18:55:31 +0530

    ✅ Java Inheritance II [java] — approach1

---
+++ solutions/java/easy/java-inheritance-2/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-inheritance-2/problem?isFullScreen=true
+// Problem     Java Inheritance II
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-06, 06:55 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+//Write your code here
+class Arithmetic {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+}
+
