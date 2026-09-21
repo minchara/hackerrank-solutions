3143783b5b1c8a8ff9326116f75ec0338766f4e5
Minchara H V <minchara48@gmail.com>
2026-09-21 12:43:11 +0530

    ✅ Java Method Overriding [java] — approach1

---
+++ solutions/java/easy/java-method-overriding/solution-approach1.java
@@ -0,0 +1,18 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-method-overriding/problem?isFullScreen=true
+// Problem     Java Method Overriding
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-21, 12:43 p.m.
+// ──────────────────────────────────────────────────
+
+
+     @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in " + getName());
+    }
+    // Write your overridden getNumberOfTeamMembers method here
+
